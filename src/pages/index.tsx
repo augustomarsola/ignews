d3f35c099b5029f9b9ee73b879afadaa8e2a741a
Augusto Marsola <augusto.marsola@live.com>
2022-05-17 22:44:28 -0300

    finishing home visualization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,36 @@
 import Head from "next/head";
+import Image from "next/image";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ínicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>
-        Hello <span>Word</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <Image
+          src="/images/avatar.svg"
+          height="521"
+          width="336"
+          alt="Girl Coding"
+        />
+      </main>
     </>
   );
 }
