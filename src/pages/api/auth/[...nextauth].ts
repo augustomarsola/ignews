691127d6a3992bd469c7e7060bca404573a0feba
Augusto Marsola <augusto.marsola@live.com>
2022-05-26 08:16:03 -0300

    Checking duplicated users on db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,33 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const userEmail = user.email;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email as string)
+                )
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email: user.email } }),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email as string)
+              )
+            )
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
